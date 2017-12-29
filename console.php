f6b21903df2108f453a6799050848045aa337da8
Patrick Teunissen <patrick@yiiandme.com>
2017-12-29 21:50:50 +0100

    Sandbox of gdax does not work so can never test the code but in production

--- console.php
+++ console.php
@@ -12,7 +12,6 @@ $client = new \GDAX\Clients\AuthenticatedClient(
         getenv('GDAX_API_KEY'), getenv('GDAX_API_SECRET'), getenv('GDAX_PASSWORD')
         );
 
-$client->setBaseURL(\GDAX\Utilities\GDAXConstants::GDAX_API_SANDBOX_URL);
 $gdaxService = new \App\Services\GDaxService($client, getenv('CRYPTOCOIN'));
 
 
