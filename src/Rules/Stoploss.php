81a269ca9d31e13be575b36459226638eb7eb212
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-08 08:38:00 +0100

    More logging

--- src/Rules/Stoploss.php
+++ src/Rules/Stoploss.php
@@ -26,13 +26,13 @@ class Stoploss
         $stoploss = (float)($stoplossPercentage / 100);
         // Hate loss
         $limitLoss = $buyprice * (1 - $stoploss);
-                
+        $profitTreshold = $buyprice * (1 + $stoploss);  
         $oldLimit = Cache::get('gdax.stoploss.'.$position_id, null);
                         
         $limit = (float)$currentprice * (1 - $stoploss); // 97 < 100 < 103, Take loss at 97 and lower
         Cache::put('gdax.stoploss.'.$position_id, $limit, 3600);
                       
-        $output->writeln('<info>Currentprice: '.$currentprice.',Bought: '. $buyprice. ', Limit: ' .$limit . ', Oldlimit: '.$oldLimit . ", Limit loss: ". $limitLoss. " Stoploss: " . $stoploss . '</info>');
+        $output->writeln('<info>Currentprice: '.$currentprice.',Bought: '. $buyprice. ', Limit: ' .$limit . ', Oldlimit: '.$oldLimit . ", Limit loss: ". $limitLoss. ", Profit treshold: ".$profitTreshold.", Stoploss: " . $stoploss . '</info>');
         
         // Take profit
         if ($limit > $buyprice && $currentprice < $oldLimit) {
